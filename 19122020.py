9e7cd777e7cd3ffe61ba190fc6f779bd121d7d3e
Pigot12 <45109176+Pigot12@users.noreply.github.com>
2020-12-22 19:31:41 +0200

    Print
    1st Course

---
+++ 19122020.py
@@ -0,0 +1,43 @@
+# Hello World
+
+
+print("Hello World")
+
+# 5 + 3 - 1
+
+print(5 + 3 - 1)    
+
+# 3 *(2+5)
+
+print(3 * (2+5))
+
+# 3 * 2
+
+print(3 * 2)
+
+#Value Name
+
+name = ("name")
+
+print("name Age ...")
+
+#Value P
+
+p = 3.14
+
+print ("p")
+
+#Value X
+
+x = (5)
+
+print (5 * 5 / x)
+
+name = "name"
+age = "14"
+color = "red"
+fivem = " "
+
+print ("my favourite color: " + color + fivem + "My name : " + name )
+
+print( ( 3* 5 ) * 3)
