a66024cf8b0ade68e5a4da90fb739cbfb092894d
Pigot12 <45109176+Pigot12@users.noreply.github.com>
2020-12-23 22:21:25 +0200

    Exercises 19-12-2020

---
+++ Exercises19122020.py
@@ -0,0 +1,20 @@
+# Print me your age and your name under that comment.
+
+print("Name:Giannis Age:x")
+
+# Print me your name and your country under that comment.
+
+#
+
+print("Name:Giannis " + "Country:Greece")
+
+# Print me how many years you need to became 18 years old by using your current age under that comment.
+# 18 - x
+
+# x = 14
+
+# intigers (5, 10)
+# String "Name, 5 + 2"    
+
+age = int(input("Tell me ur age"))
+print(age)
