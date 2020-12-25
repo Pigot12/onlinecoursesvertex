8276b600ecfaca56f8efb7ba951135157786bdb7
Pigot12 <45109176+Pigot12@users.noreply.github.com>
2020-12-25 22:08:16 +0200

    2nd part of 2 course

---
+++ 211220202.py
@@ -0,0 +1,16 @@
+# otan o User einai 18 kai panw! na mporei na mpei sto site 
+# otan o User einai katw ton 18 na tou emfanizei ena mnm se posa xronia na epistrepsei
+
+# name = input("Tell me your name ")
+age = int(input("Tell me your age "))
+
+# if=filtro.
+if(age >= 18):
+    print("Congratulations.You pass the test.")
+else:
+    print("We don't allow you the access.")
+
+    print("Allow in     ")
+    print (18 - age)
+    print("years")
+    # 18 - age
