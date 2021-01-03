46ad5ff6137ff525577eca98130498dc5a31744b
Pigot12 <45109176+Pigot12@users.noreply.github.com>
2021-01-03 20:51:16 +0200

    Add files via upload

---
+++ 312021.py
@@ -0,0 +1,43 @@
+#if and for statmend
+
+#if 23 >= 18:
+#    print("Accepted")
+#block 
+
+#if (23 >= 18):
+#    print("A")
+#    print("C")
+#    print("A")
+#    print("B")
+
+#if (2 >= 18):
+#    print("A")
+#    print("C")
+#    print("A")
+#    print("B")
+
+#years = [2000 , 2001 , 2005 , 2010 , 2020 , 2025]
+
+#if (years[0] == 2001):
+#    print("a")
+#if (years[1] == 2002):
+#    print("b")
+#if (years[2] == 2003):
+#    print("c")
+#if (years[3] == 2004):
+#    print("d")
+#else:
+#    print(2020 + x)
+
+#loops
+years = [2000 , 2001 , 2005 , 2010 , 2020 , 2025]
+
+for xronia in years:
+    if(xronia >= 2005):
+        print("Hawkaras")
+    elif(xronia >= 2005):
+        print(xronia , "Hawkaras")
+    else:
+        print(xronia , "Lightaras")
+
+
