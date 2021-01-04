b89506aa9bcf5c75b29b79c91ffbe2ed0912967f
Pigot12 <45109176+Pigot12@users.noreply.github.com>
2021-01-04 17:34:26 +0200

    5nd course

---
+++ 3120212.py
@@ -0,0 +1,18 @@
+# pairneis input 2 ari8mous
+# input metavlith(*, +, -, /)
+
+number1 = int(input(print("Ton ariumo sas PARAKALW")))
+number2 = int(input(print("Enan allo ariumo PARAKALWW")))
+
+variable = input(print("Kai teletaio thn metablth sas PARAKALW"))
+
+if(variable == '+'):
+    print(number1 + number2)
+elif(variable == '-'):
+    print(number1 - number2)
+elif(variable == '*'):
+    print(number1 * number2)
+elif(variable == '/'):
+    print(number1 / number2)
+else:
+    print("Jana prospauhse")
